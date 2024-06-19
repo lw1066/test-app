6381b085f522aad9e2a47d182e7833dc6568f487
lw1066 <lewis_webster@hotmail.com>
2024-06-19 10:59:22 +0100

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "example.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        pathname: "/v0/b/test-pp-njs.appspot.com/o/**",
-      },
-    ],
+    domains: ["firebasestorage.googleapis.com"],
   },
 };
 

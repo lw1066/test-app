920f36d7e8ab444c8a2eef6190ed17dfe573fa29
lw1066 <lewis_webster@hotmail.com>
2024-07-07 09:44:22 +0100

    added links to submissions page

--- src/app/Submissions/page.js
+++ src/app/Submissions/page.js
@@ -1,6 +1,13 @@
 import classes from "../components/Library.module.css";
 
 function Page() {
+  const submissionGuideUrl =
+    "https://firebasestorage.googleapis.com/v0/b/test-pp-njs.appspot.com/o/MS%20Submission%20Guidelines.pdf?alt=media&token=9132d7c9-35b6-4b15-b534-1e3dbf1a0123";
+  const googleFormsLink =
+    "https://docs.google.com/forms/d/e/1FAIpQLSc7ncjkKopry6gRQjjcRgjSIfpZZhXmDvsfTjKR5YTbAEoxFQ/viewform?usp=sf_link";
+  const emailLink =
+    "mailto:submissions@perceptiapress.com?subject=Submission &body=Thank%20you%20for%20submitting%20to%20us%20we%20will%20be%20in%20touch.";
+
   return (
     <>
       <div className={classes.bannerImage}>
@@ -23,17 +30,28 @@ function Page() {
           Press. We are always interested in looking at new projects.
         </p>
         <p className={classes.pageDescriptionSubmissions}>
-          Please download our Submissions Guidelines and consider your
-          submission. Then complete this proposal form in as much detail as
-          possible.
+          Please download our{" "}
+          <a
+            href={submissionGuideUrl}
+            className={classes.linkStyle}
+            target="blank"
+          >
+            Submissions Guidelines
+          </a>{" "}
+          and consider your submission. Then complete this{" "}
+          <a
+            href={googleFormsLink}
+            className={classes.linkStyle}
+            target="blank"
+          >
+            proposal form
+          </a>{" "}
+          in as much detail as possible.
         </p>
         <p className={classes.pageDescriptionSubmissions}>
           Finally, send a sample unit (if available) and recent CV including any
           publishing experience to{" "}
-          <a
-            className={classes.linkStyle}
-            href="mailto:submissions@perceptiapress.com?subject=Submission &body=Thank%20you%20for%20submitting%20to%20us%20we%20will%20be%20in%20touch."
-          >
+          <a className={classes.linkStyle} href={emailLink}>
             submissions@perceptiapress.com
           </a>
           . We look forward to seeing your work.
